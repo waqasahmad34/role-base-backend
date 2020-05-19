3e2f72fccebf1fef60094b8d418bbe9f398b84d5
waqasahmad34 <48016736+waqasahmad34@users.noreply.github.com>
2020-05-19 09:57:27 +0500

    update

--- config/db.js
+++ config/db.js
@@ -1,21 +1,21 @@
-const mongoose = require("mongoose");
-const config = require("config");
-const db = config.get("mongoURI");
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-      useUnifiedTopology: true
-    });
-    console.log("MongoDB Connected");
-  } catch (error) {
-    console.error(error.message);
-    // Exit process with failure
-    process.exit(1);
-  }
+	try {
+		await mongoose.connect(process.env.MONGO_URI || db, {
+			useNewUrlParser: true,
+			useFindAndModify: false,
+			useCreateIndex: true,
+			useUnifiedTopology: true
+		});
+		console.log('MongoDB Connected');
+	} catch (error) {
+		console.error(error.message);
+		// Exit process with failure
+		process.exit(1);
+	}
 };
 
 module.exports = connectDB;
